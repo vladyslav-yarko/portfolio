ab0b9ba4fd8b3490f934ab3182a67f91cf3a99ef
vladyslav-yarko <vladislavv44444@gmail.com>
2025-09-02 00:52:12 +0300

    chore(frontend): configure eslint config

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,29 +1,34 @@
 import js from '@eslint/js'
 import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import pluginReact from 'eslint-plugin-react'
+import path from 'path'
+import { defineConfig } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist']),
-  {
-    files: ['**/*.{js,jsx}'],
-    extends: [
-      js.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-    ],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-      parserOptions: {
-        ecmaVersion: 'latest',
-        ecmaFeatures: { jsx: true },
-        sourceType: 'module',
-      },
-    },
-    rules: {
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
-    },
-  },
+	{
+		files: ['**/*.{js,jsx}'],
+		plugins: {
+			js,
+			react: pluginReact,
+			'unused-imports': require('eslint-plugin-unused-imports'),
+		},
+		extends: ['js/recommended', 'plugin:react/recommended'],
+		languageOptions: { globals: globals.browser },
+		rules: {
+			'no-unused-vars': 'warn',
+			'unused-imports/no-unused-imports': 'warn',
+			'no-console': 'warn',
+			'react/prop-types': 'off',
+			'react/react-in-jsx-scope': 'off', // React 17+ JSX
+		},
+		settings: {
+			'import/resolver': {
+				alias: {
+					map: [['@', './src']],
+					extensions: ['.js', '.jsx'],
+				},
+			},
+			react: { version: 'detect' },
+		},
+	},
 ])
