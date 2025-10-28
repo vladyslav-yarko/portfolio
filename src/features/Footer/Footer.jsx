8abfeec5e5ab59a2236c1f220b1f323e90022abb
vladyslav-yarko <vladislavv44444@gmail.com>
2025-10-28 20:14:47 +0200

    fix: Link target attribute

--- src/features/Footer/Footer.jsx
+++ src/features/Footer/Footer.jsx
@@ -1,8 +1,13 @@
+import Github from "./components/Github"
+import LinkedIn from "./components/LinkedIn"
+
+
 export default function Footer() {
 
     return (
         <div>
-            
+            <Github />
+            <LinkedIn />
         </div>
     )
 }
--- src/ui/LinkNewTabComponent.jsx
+++ src/ui/LinkNewTabComponent.jsx
@@ -4,10 +4,8 @@ import Link from "next/link"
 export default function LinkNewTabComponent({ link, children }) {
 
     return (
-        <Link href={link}>
-            <a target="_blank">
-                {children}
-            </a>
+        <Link target="_blank" href={link}>
+            {children}
         </Link>
     )
 }
