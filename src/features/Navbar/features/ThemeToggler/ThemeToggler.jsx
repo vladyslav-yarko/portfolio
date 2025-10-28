e4ae0fd35d0b4a1d2c8ce051ac519c4361c8641b
vladyslav-yarko <vladislavv44444@gmail.com>
2025-10-28 18:45:10 +0200

    feat: update ThemeToggler

--- src/features/Navbar/features/ThemeToggler/ThemeToggler.jsx
+++ src/features/Navbar/features/ThemeToggler/ThemeToggler.jsx
@@ -1,8 +1,41 @@
+"use client"
+
+import { useEffect, useState } from "react"
+
+import Sun from "./components/Sun"
+import Moon from "./components/Moon"
+
+
 export default function ThemeToggler() {
+    const [ theme, setTheme ] = useState("light")
+
+    useEffect(() => {
+        const localTheme = localStorage.getItem("portfolioTheme")
+        setTheme(localTheme)
+    }, [])
+
+    useEffect(() => {
+        if (["dark", "light"].includes(theme)) {
+            document.documentElement.setAttribute('data-theme', theme)
+            localStorage.setItem('data-theme', theme)
+        } else {
+            setTheme("light")
+        }
+    }, [theme])
+
+    const updateTheme = () => {
+        setTheme(prev => (prev === 'light' ? 'dark' : 'light'))
+    }
 
     return (
-        <div>
-            
+        <div onClick={() => updateTheme()}>
+            {
+                theme === "dark" ? (
+                    <Moon />
+                ) : (
+                    <Sun />
+                )
+            }
         </div>
     )
 }
--- src/features/Navbar/features/ThemeToggler/components/Moon.jsx
+++ src/features/Navbar/features/ThemeToggler/components/Moon.jsx
@@ -1,4 +1,4 @@
-export function Moon(props) {
+export default function Moon(props) {
 	return (
 		<svg
 			xmlns='http://www.w3.org/2000/svg'
