74f63b3c3fc541b009ad1afe3c8fced0c2389c26
vladyslav-yarko <vladislavv44444@gmail.com>
2025-10-28 19:13:08 +0200

    feat: update NavHamburgerMenu feature

--- src/features/Navbar/features/NavHamburger/features/NavHamburgerMenu.jsx
+++ src/features/Navbar/features/NavHamburger/features/NavHamburgerMenu.jsx
@@ -5,7 +5,11 @@ export default function NavHamburgerMenu() {
 
     return (
         <div>
-            
+            <NavHamburgerMenuItem title="Home" link="/"/>
+            <NavHamburgerMenuItem title="About" link="/about"/>
+            <NavHamburgerMenuItem title="Stack" link="/stack"/>
+            <NavHamburgerMenuItem title="Projects" link="/projects"/>
+            <NavHamburgerMenuItem title="Contact" link="/contact"/>
         </div>
     )
 }
