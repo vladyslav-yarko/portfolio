6abb27f61a6303fd207b15b63a0632dcba8f8169
vladyslav-yarko <vladislavv44444@gmail.com>
2025-10-30 19:38:08 +0200

    feat: customize entire color theme

--- src/features/Navbar/features/NavHamburger/components/NavHamburgerIcon.jsx
+++ src/features/Navbar/features/NavHamburger/components/NavHamburgerIcon.jsx
@@ -1,6 +1,6 @@
 export default function NavHamburgerIcon(props) {
 	return (
-		<div className={`hamburgerIcon ${props.menuOpened ? "text-blue-500" : ""}`}>
+		<div className={`hamburgerIcon ${props.menuOpened ? "text-accent dark:text-accent-dark" : ""}`}>
 			<svg
 				xmlns='http://www.w3.org/2000/svg'
 				width='100%'
