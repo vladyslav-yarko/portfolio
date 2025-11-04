3117ee579dbf38753df2eceaf59cfd3c28c532d6
vladyslav-yarko <vladislavv44444@gmail.com>
2025-11-04 20:54:10 +0200

    feat: add base loading state style

--- src/ui/LoadingState.jsx
+++ src/ui/LoadingState.jsx
@@ -1,21 +1,23 @@
 export default function LoadingState(props) {
 	return (
-		<svg
-			xmlns='http://www.w3.org/2000/svg'
-			width='100%'
-			height='100%'
-			viewBox='0 0 48 48'
-			{...props}
-		>
-			{/* Icon from IconPark Outline by ByteDance - https://github.com/bytedance/IconPark/blob/master/LICENSE */}
-			<path
-				fill='none'
-				stroke='currentColor'
-				strokeLinecap='round'
-				strokeLinejoin='round'
-				strokeWidth='4'
-				d='M24 4v4m10-1.32l-2 3.464M41.32 14l-3.464 2M44 24h-4m1.32 10l-3.464-2M34 41.32l-2-3.464M24 44v-4m-10 1.32l2-3.464M6.68 34l3.464-2M4 24h4M6.68 14l3.464 2M14 6.68l2 3.464'
-			/>
-		</svg>
+		<div className='text-blue-700'>
+			<svg
+				xmlns='http://www.w3.org/2000/svg'
+				width='100%'
+				height='100%'
+				viewBox='0 0 48 48'
+				{...props}
+			>
+				{/* Icon from IconPark Outline by ByteDance - https://github.com/bytedance/IconPark/blob/master/LICENSE */}
+				<path
+					fill='none'
+					stroke='currentColor'
+					strokeLinecap='round'
+					strokeLinejoin='round'
+					strokeWidth='4'
+					d='M24 4v4m10-1.32l-2 3.464M41.32 14l-3.464 2M44 24h-4m1.32 10l-3.464-2M34 41.32l-2-3.464M24 44v-4m-10 1.32l2-3.464M6.68 34l3.464-2M4 24h4M6.68 14l3.464 2M14 6.68l2 3.464'
+				/>
+			</svg>
+		</div>
 	)
 }
