a402f9a6d173cdfc271a7b7d4b59c2027cd26e29
vladyslav-yarko <vladislavv44444@gmail.com>
2025-11-05 20:15:43 +0200

    feat: add full not found page

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -1,8 +1,25 @@
+import NotFoundIcon from "@/ui/NotFoundIcon"
+import ButtonPrimary from "@/ui/ButtonPrimary"
+import LinkComponent from "@/ui/LinkComponent"
+
+
 export default function NotFoundPage() {
 
 	return (
 		<div>
-			<h2>Page not found</h2>
+			<div className='flex justify-center items-center'>
+				<NotFoundIcon className='notFoundIcon' />
+				<h2>Page not found</h2>
+			</div>
+			<div className='flex justify-center items-center'>
+				<div className='notFoundButton'>
+					<ButtonPrimary>
+						<h2>
+							<LinkComponent title='Return to home' link='/' onClick={null} />
+						</h2>
+					</ButtonPrimary>
+				</div>
+			</div>
 		</div>
 	)
 }
--- src/ui/NotFoundIcon.jsx
+++ src/ui/NotFoundIcon.jsx
@@ -1,10 +1,10 @@
 export default function NotFoundIcon(props) {
 	return (
-		<div className="text-red-500">
+		<div className='text-red-500'>
 			<svg
 				xmlns='http://www.w3.org/2000/svg'
-				width='1em'
-				height='1em'
+				width='100%'
+				height='100%'
 				viewBox='0 0 24 24'
 				{...props}
 			>
