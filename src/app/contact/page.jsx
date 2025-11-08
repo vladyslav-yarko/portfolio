c9dfbffd85753840bdf44128a9af9c650a72dc0a
vladyslav-yarko <vladislavv44444@gmail.com>
2025-11-08 11:39:46 +0200

    feat: add base ContactHeadline feature

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,10 +1,12 @@
 import ContactBody from "@/features/ContactBody"
+import ContactHeadline from "@/features/ContactHeadline"
 
 
 export default function ContactPage() {
 
     return (
         <div>
+            <ContactHeadline />
             <ContactBody />
         </div>
     )
---
+++ src/features/ContactHeadline/ContactHeadline.jsx
@@ -0,0 +1,8 @@
+export default function ContactHeadline() {
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
